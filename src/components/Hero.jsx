a63c1a1cfc14121cbebaea0887c7c2c8f8399ec5
Anika <anika@medinatech.co>
2023-11-22 15:40:51 +0600

    search box done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import beachVid from '../assets/beachVid.mp4'
+import React from "react";
+import beachVid from "../assets/beachVid.mp4";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen relative'>
-        <video className='w-full h-full object-cover' autoPlay loop muted src={beachVid}></video>
-    
-    <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
-    
-    </div>
-  )
-}
+    <div className="w-full h-screen relative">
+      <video
+        className="w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        src={beachVid}
+      ></video>
 
-export default Hero
+      <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
+        <h1>First Class Travel</h1>
+        <h2 className="py-4">Top 1% Locatoins Worldwide</h2>
+        <form
+          
+          className='flex justify-between items-center mx-auto max-w-[700px] border p-1
+          rounded-md text-black bg-gray-100/90'
+        >
+          <div>
+            <input
+              className="bg-transparent focus:outline-none w-[200px] md:w-[600px] font-[Poppins]"
+              type="text"
+              placeholder="Search Destinations"
+            />
+          </div>
+
+          <div>
+            <button>
+              <AiOutlineSearch
+                size={20}
+                className="icon"
+                style={{ color: "#fff" }}
+              />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
