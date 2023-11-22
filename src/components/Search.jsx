794f7b330897b645fdd4de3b929f530c44a3b2ec
unknown <anikatahsinmeem@gmail.com>
2023-11-22 22:45:41 +0600

    date form done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -24,23 +24,60 @@ const Search = () => {
         </div>
         <div className="grid sm:grid-cols-2 gap-8 py-4">
           <div className="flex flex-col lg:flex-row items-center text-center">
-            <button><RiCustomerServiceFill size={50} /> </button>
+            <button>
+              <RiCustomerServiceFill size={50} />
+            </button>
             <div>
-                <h3>LEADING SERVICE</h3>
-                <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+              <h3 className="py-2">LEADING SERVICE</h3>
+              <p className="py-1">
+                ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW
+              </p>
             </div>
           </div>
           <div className="flex flex-col lg:flex-row items-center text-center">
-            <button><MdTravelExplore size={50} /> </button>
+            <button>
+              <MdTravelExplore size={50} />
+            </button>
             <div>
-                <h3>LEADING SERVICE</h3>
-                <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+              <h3 className="py-2">LEADING SERVICE</h3>
+              <p className="py-1 ">
+                ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW
+              </p>
             </div>
           </div>
-          <div></div>
         </div>
       </div>
-      <div>right</div>
+
+      <div>
+        <div className="border text-center">
+          <p className="pt-2">GET AN ADDITIONAL 10% OFF</p>
+          <p className="py-4">12 HOURS LEFT</p>
+
+          <p className="bg-gray-800 text-gray-200 py-2 my-4 mx-4">
+            BOOK NOW AND SAVE
+          </p>
+        </div>
+        <form className="w-full">
+          <div className="flex flex-col my-2">
+            <label>Destination</label>
+            <select className="border rounded-md p-2">
+              <option value="">Grand Antigua</option>
+              <option value="">Key West</option>
+              <option value="">Maldives</option>
+              <option value="">Cozumel</option>
+            </select>
+          </div>
+          <div className="flex flex-col my-2">
+            <label>Check-In</label>
+            <input className="border rounded-md p-2" type="date" />
+          </div>
+          <div className="flex flex-col my-2">
+            <label>Check-Out</label>
+            <input className="border rounded-md p-2" type="date" />
+          </div>
+          <button className="w-full my-4">Rates & Availabilities</button>
+        </form>
+      </div>
     </div>
   );
 };
