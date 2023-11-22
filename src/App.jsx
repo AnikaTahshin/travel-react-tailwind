c758165908a5e188fb09ce5fca7402caa7c87aed
Anika <anika@medinatech.co>
2023-11-22 16:03:51 +0600

    grid image shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Destination from "./components/Destination"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <Destination />
       </div>
     </>
   )
